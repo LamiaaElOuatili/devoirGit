4bafcf8d03cb5344f6d62bc3b4e410a31db55f46
Lamiaa El Ouatili <lamiaaelouatili@hotmail.com>
2024-04-24 10:19:43 +0100

    first commit

---
+++ src/main/java/com/example/devoir_git/DevoirGitApplication.java
@@ -0,0 +1,13 @@
+package com.example.devoir_git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevoirGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevoirGitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/devoir_git/DevoirGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devoir_git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevoirGitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
